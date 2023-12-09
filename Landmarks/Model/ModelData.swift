32440f2193cff35539f8836c6000f0dda8784726
Charlene Hoareau <charlene.hoareau@outlook.com>
2023-12-09 11:11:14 +0100

    complete category view

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -12,6 +12,10 @@ class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     //@Published var hikes: [Hike] = load("hikeData.json")
     
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
+    }
+    
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarks,
